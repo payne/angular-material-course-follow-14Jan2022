a22c5c3f69481ba4e816a4c0d0e3cd2b7c841f9b
Matt Payne <Payne@MattPayne.org>
2022-01-14 22:55:07 +0000

    wip

--- amc1/src/app/app.module.ts
+++ amc1/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSliderModule,MatSidenavModule,MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [],
