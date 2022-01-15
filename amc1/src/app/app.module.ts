0310f1626f9ee1c9e12e296c160dcd2fbcc71808
Matt Payne <Payne@MattPayne.org>
2022-01-15 22:58:35 +0000

    copied from source materials
    https://github.com/angular-university/angular-material-course/tree/1-navigation-and-containers-finished

--- amc1/src/app/app.module.ts
+++ amc1/src/app/app.module.ts
@@ -15,9 +15,18 @@ import {MatListModule} from '@angular/material/list';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    TopMenuComponent,
+    CoursesCardListComponent
   ],
   imports: [
     BrowserModule,
