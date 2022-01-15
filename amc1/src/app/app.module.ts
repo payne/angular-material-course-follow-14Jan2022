e1bdb36cbd00ea4ef30a87b2f2eb3bccfdde1374
Matt Payne <Payne@MattPayne.org>
2022-01-15 22:39:17 +0000

    Copied from https://github.com/angular-university/angular-material-course/tree/1-navigation-and-containers-finished

--- amc1/src/app/app.module.ts
+++ amc1/src/app/app.module.ts
@@ -5,9 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
@@ -18,6 +23,13 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     MatSliderModule,MatSidenavModule,MatToolbarModule,MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTabsModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
     BrowserAnimationsModule
   ],
   providers: [],
